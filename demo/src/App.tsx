8455753c251ecca033053c70ec44ea8f10bbdeb7
iamdog2k <superwwj@hotmail.com>
2024-01-13 11:10:19 +0900

    first look

---
+++ demo/src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import styled from 'styled-components';
+
+const Title = styled.h1`
+  font-size: 1.5em;
+  color: red;
+`;
+
+function App() {
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default App;
